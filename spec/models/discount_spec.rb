b271e4cb705fb4ded71814edf85f2a6f924ad64e
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-09 16:41:48 -0700

    Set up associations in models

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
   describe "validations" do
-    it { should validate_presence_of :percent }
-    it { should validate_presence_of :quantity }
+    it { should validate_presence_of :percentage }
+    it { should validate_presence_of :threshold }
   end
+  
   describe "relationships" do
     it { should belong_to :merchant }
   end
