83e9432ef513daf6b5707ed7657a36579e544aaf
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-10 16:33:26 -0700

    Write tests for user story 6

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -100,4 +100,27 @@ RSpec.describe "invoices show" do
     end
   end
 
+  describe "User Story 6" do
+    before :each do
+      @discount1 = @merchant1.discounts.create!(percentage: 15, threshold: 5)
+      @discount2 = @merchant1.discounts.create!(percentage: 20, threshold: 10)
+      @discount3 = @merchant1.discounts.create!(percentage: 30, threshold: 15)
+      @discount4 = @merchant2.discounts.create!(percentage: 10, threshold: 20)
+      @invoice_8 = Invoice.create!(customer_id: @customer_6.id, status: 2)
+      @ii_12 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_1.id, quantity: 20, unit_price: 10, status: 2)
+      @ii_13 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_3.id, quantity: 10, unit_price: 5, status: 2)
+    end
+    it "shows total revenue for my merchant from this invoice" do
+      visit merchant_invoice_path(@merchant1, @invoice_8)
+      
+      expect(page).to have_content("Revenue Before Discounts: #{@invoice_8.total_revenue}")
+    end
+
+    it "shows total discounted revenue for my merchant" do
+      visit merchant_invoice_path(@merchant1, @invoice_8)
+
+      expect(page).to have_content("Revenue After Discounts: 180")
+    end
+  end
+
 end
