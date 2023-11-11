ca48c8015fb32fe7bef84b26aba730aa60cd52c2
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-10 17:58:38 -0700

    Write feature tests for user story 7

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -100,7 +100,7 @@ RSpec.describe "invoices show" do
     end
   end
 
-  describe "User Story 6" do
+  describe "Final Solo" do
     before :each do
       @discount1 = @merchant1.discounts.create!(percentage: 15, threshold: 5)
       @discount2 = @merchant1.discounts.create!(percentage: 20, threshold: 10)
@@ -109,18 +109,43 @@ RSpec.describe "invoices show" do
       @invoice_8 = Invoice.create!(customer_id: @customer_6.id, status: 2)
       @ii_12 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_1.id, quantity: 20, unit_price: 10, status: 2)
       @ii_13 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_3.id, quantity: 10, unit_price: 5, status: 2)
+      @ii_14 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_4.id, quantity: 2, unit_price: 5, status: 2)
     end
-    it "shows total revenue for my merchant from this invoice" do
-      visit merchant_invoice_path(@merchant1, @invoice_8)
+
+    describe "User Story 6" do
+      it "shows total revenue for my merchant from this invoice" do
+        visit merchant_invoice_path(@merchant1, @invoice_8)
+        
+        expect(page).to have_content("Revenue Before Discounts: #{@invoice_8.total_revenue}")
+      end
+
+      it "shows total discounted revenue for my merchant" do
+        visit merchant_invoice_path(@merchant1, @invoice_8)
+
+        expect(page).to have_content("Revenue After Discounts: 190")
+      end
+    end
+
+    describe "User Story 7" do
+      it "shows discounts applied per invoice_item" do
+        visit merchant_invoice_path(@merchant1, @invoice_8)
+        
+        within("#the-status-#{@ii_12.id}") do
+          expect(page).to have_link("Discount ##{@discount3.id}")
+        
+          click_link("Discount ##{@discount3.id}")
+        end
       
-      expect(page).to have_content("Revenue Before Discounts: #{@invoice_8.total_revenue}")
-    end
+        expect(current_path).to eq(merchant_discount_path(@merchant1, @discount3))
+      end
 
-    it "shows total discounted revenue for my merchant" do
-      visit merchant_invoice_path(@merchant1, @invoice_8)
+      it "shows none applicable when no discounts are available" do
+        visit merchant_invoice_path(@merchant1, @invoice_8)
 
-      expect(page).to have_content("Revenue After Discounts: 180")
+        within("#the-status-#{@ii_14.id}") do
+          expect(page).to have_content("None Applicable")
+        end
+      end
     end
   end
-
 end
