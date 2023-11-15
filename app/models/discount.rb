bfea3693960e70c0c3bb44dcaccd1f47a711b245
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-14 18:12:05 -0700

    Add more through relationships to access discounts from invoiceitems

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,7 +2,8 @@ class Discount < ApplicationRecord
   validates :percentage, presence: true, numericality: true, inclusion: { in: 1..100 }
   validates :threshold, presence: true, numericality: true
   belongs_to :merchant
-
+  has_many :invoice_items, through: :merchant
+  
   def percent_multiplied
     (100 - percentage) * 0.01
   end
